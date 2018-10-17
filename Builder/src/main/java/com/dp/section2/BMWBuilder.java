a05e62456e5df48cd1d113eb30c117082660f0d3
陆地 <ludiabc@renrenche.com>
2018-10-17 17:09:03 +0800

    建造者模式

---
+++ Builder/src/main/java/com/dp/section2/BMWBuilder.java
@@ -0,0 +1,18 @@
+package com.dp.section2;
+
+import java.util.ArrayList;
+
+public class BMWBuilder extends CarBuilder {
+	private BMWModel bmw = new BMWModel();
+	
+	@Override
+	public CarModel getCarModel() {
+		return this.bmw;
+	}
+
+	@Override
+	public void setSequence(ArrayList<String> sequence) {
+		this.bmw.setSequence(sequence);
+	}
+
+}
