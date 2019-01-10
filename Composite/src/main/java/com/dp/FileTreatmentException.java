316adffd1ae82fe3f1e63a6082742206de6cab34
陆地 <ludiabc@renrenche.com>
2019-01-10 11:33:52 +0800

    Composite模式

---
+++ Composite/src/main/java/com/dp/FileTreatmentException.java
@@ -0,0 +1,9 @@
+package com.dp;
+
+public class FileTreatmentException extends RuntimeException {
+	public FileTreatmentException() {
+    }
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
